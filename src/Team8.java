552d44539778822574cd890283e3312f41d7fd45
Hardik Sharma <hardiksharmah73@gmail.com>
2023-10-02 14:11:25 +0530

    hope it works

--- src/Team8.java
+++ src/Team8.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.lang.Math;
 import java.util.Scanner;
@@ -18,13 +19,19 @@ import genius.core.boaframework.SortedOutcomeSpace;
 
 class Logistic {
 
-    /** the learning rate */
+    /**
+     * the learning rate
+     */
     private double rate;
 
-    /** the weight to learn */
+    /**
+     * the weight to learn
+     */
     private double[] weights;
 
-    /** the number of iterations */
+    /**
+     * the number of iterations
+     */
     private int ITERATIONS = 3000;
 
     public Logistic(int n) {
@@ -37,17 +44,17 @@ class Logistic {
     }
 
     public void train(List<Instance> instances) {
-        for (int n=0; n<ITERATIONS; n++) {
+        for (int n = 0; n < ITERATIONS; n++) {
             double lik = 0.0;
-            for (int i=0; i<instances.size(); i++) {
+            for (int i = 0; i < instances.size(); i++) {
                 int[] x = instances.get(i).x;
                 double predicted = classify(x);
                 int label = instances.get(i).label;
-                for (int j=0; j<weights.length; j++) {
+                for (int j = 0; j < weights.length; j++) {
                     weights[j] = weights[j] + rate * (label - predicted) * x[j];
                 }
                 // not necessary for learning
-                lik += label * Math.log(classify(x)) + (1-label) * Math.log(1- classify(x));
+                lik += label * Math.log(classify(x)) + (1 - label) * Math.log(1 - classify(x));
             }
             System.out.println("iteration: " + n + " " + Arrays.toString(weights) + " mle: " + lik);
         }
@@ -55,7 +62,7 @@ class Logistic {
 
     private double classify(int[] x) {
         double logit = .0;
-        for (int i=0; i<weights.length;i++)  {
+        for (int i = 0; i < weights.length; i++) {
             logit += weights[i] * x[i];
         }
         return sigmoid(logit);
@@ -76,7 +83,7 @@ class Logistic {
         Scanner scanner = null;
         try {
             scanner = new Scanner(new File(file));
-            while(scanner.hasNextLine()) {
+            while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 if (line.startsWith("#")) {
                     continue;
@@ -85,9 +92,9 @@ class Logistic {
 
                 // skip first column and last column is the label
                 int i = 1;
-                int[] data = new int[columns.length-2];
-                for (i=1; i<columns.length-1; i++) {
-                    data[i-1] = Integer.parseInt(columns[i]);
+                int[] data = new int[columns.length - 2];
+                for (i = 1; i < columns.length - 1; i++) {
+                    data[i - 1] = Integer.parseInt(columns[i]);
                 }
                 int label = Integer.parseInt(columns[i]);
                 Instance instance = new Instance(label, data);
@@ -99,6 +106,7 @@ class Logistic {
         }
         return dataset;
     }
+
     public static void main(String... args) throws FileNotFoundException {
         List<Instance> instances = readDataSet("dataset.txt");
         Logistic logistic = new Logistic(5);
@@ -113,8 +121,7 @@ class Logistic {
 
 }
 
-public class Team8 extends AbstractNegotiationParty
-{
+public class Team8 extends AbstractNegotiationParty {
     private Bid lastOffer;
     // Stores the last offer that was proposed
     // by the opponent.
@@ -127,9 +134,12 @@ public class Team8 extends AbstractNegotiationParty
     // at any point. This is possible since we k=now the random
     // agent stops after 90% of the deadline passes.
 
+    private Logistic logReg;
+
+    private List<Logistic.Instance> instances = new ArrayList<Logistic.Instance>();
+
     @Override
-    public void init(NegotiationInfo info)
-    {
+    public void init(NegotiationInfo info) {
         super.init(info);
 
         SortedOutcomeSpace outcomeSpace = new SortedOutcomeSpace(info.getUtilitySpace());
@@ -138,25 +148,33 @@ public class Team8 extends AbstractNegotiationParty
         // our agent is going to propose.
 
         double totalRun = info.getDeadline().getValue();
-        double expectedRun = 0.9*totalRun;
+        double expectedRun = 0.9 * totalRun;
 
-        utilityThreshold = Math.pow(utilitySpace.getDiscountFactor(), expectedRun/totalRun);
+        utilityThreshold = Math.pow(utilitySpace.getDiscountFactor(), expectedRun / totalRun);
         utilityThreshold = utilityThreshold * getUtility(constantBid);
         // Now utility threshold contains the minimum bid our agent gets
         // as long as there isn't any disagreement.
 
-        this.oneHotEncoder();
+        int totalValues = 0;
+        List<Issue> allIssues = utilitySpace.getDomain().getIssues();
+
+        for (Issue rawIssue : allIssues) {
+            IssueDiscrete issue = (IssueDiscrete) rawIssue;
+            totalValues += issue.getNumberOfValues();
+        }
+
+        this.logReg = new Logistic(totalValues);
+
     }
 
     /*
-    * Checks if the proposed bid has utility greater than
-    * what we eventually will have at the end, and accepts
-    * if that is the case.
-    */
+     * Checks if the proposed bid has utility greater than
+     * what we eventually will have at the end, and accepts
+     * if that is the case.
+     */
     @Override
-    public Action chooseAction(List<Class<? extends Action>> possibleActions)
-    {
-        if ( lastOffer != null) {
+    public Action chooseAction(List<Class<? extends Action>> possibleActions) {
+        if (lastOffer != null) {
             if (getUtilityWithDiscount(lastOffer) >= utilityThreshold) {
                 return new Accept(getPartyId(), lastOffer);
                 // If the opponent proposes a bid that
@@ -165,40 +183,68 @@ public class Team8 extends AbstractNegotiationParty
             }
         }
 
-        return new Offer(getPartyId(),constantBid );
+        this.logReg.train(this.instances);
+
+        // call a function that will iterate over all possible bids
+        // and then select one with maximum score.
+
+        this.instances.add(new Logistic.Instance(0, oneHotEncoder(constantBid)));
+        return new Offer(getPartyId(), constantBid);
     }
 
     /*
-    * Stores the received bid in a variable
-    * */
+     * Stores the received bid in a variable
+     * */
     @Override
-    public void receiveMessage(AgentID sender, Action action)
-    {
-        if (action instanceof Offer)
-        {
+    public void receiveMessage(AgentID sender, Action action) {
+        if (action instanceof Offer) {
             lastOffer = ((Offer) action).getBid();
         }
+
+        this.instances.add(new Logistic.Instance(1, oneHotEncoder(lastOffer)));
     }
 
     /*
-    * Refuses to elaborate further.
-    * */
+     * Refuses to elaborate further.
+     * */
     @Override
-    public String getDescription()
-    {
+    public String getDescription() {
         return "Refusing to elaborate further.";
     }
 
-    public void oneHotEncoder( ){
+    public int[] oneHotEncoder(Bid thisBid) {
         List<Issue> allIssues = utilitySpace.getDomain().getIssues();
-        System.out.println("Got Issue Array Length :");
-        System.out.println(allIssues);
 
-        for ( Issue issue : allIssues ){
-            IssueDiscrete dIssue = (IssueDiscrete) issue;
-            for (ValueDiscrete val : dIssue.getValues() ) {
-                System.out.println(dIssue.getValueIndex(val));
+        HashMap<Integer, Value> valueMap = thisBid.getValues();
+        List<Integer> encoding = new ArrayList<>();
+
+        for (Issue rawIssue : allIssues) {
+            IssueDiscrete issue = (IssueDiscrete) rawIssue;
+            Integer issueID = issue.getNumber();
+
+            int valueID = issue.getValueIndex((ValueDiscrete) valueMap.get(issueID));
+            int numValues = issue.getNumberOfValues();
+
+            for (int i = 0; i < numValues; i++) {
+                if (i == valueID) {
+                    encoding.add(1);
+                } else {
+                    encoding.add(0);
+                }
             }
         }
+
+//        System.out.println("-------------------------------------");
+//        for (int val : encoding) {
+//            System.out.print(val);
+//        }
+
+        int[] x = new int[encoding.size()];
+
+        for (int i = 0; i < encoding.size(); i++) {
+            x[i] = encoding.get(i);
+        }
+
+        return x;
     }
 }
